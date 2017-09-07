73739c14e9224deb15323ecf49b29dd4f823e28c
coleworsley <cole.s.worsley@gmail.com>
2017-09-06 20:34:16 -0600

    add social media migration

---
+++ db/migrations/20170906202427_social-media.js
@@ -0,0 +1,24 @@
+
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTable('social_media', (table) => {
+    table.increments('id').primary();
+    table.string('yelp');
+    table.string('instagram');
+    table.string('twitter');
+    table.string('pinterest');
+    table.string('facebook');
+  }),
+
+  knex.schema.table('location', (table) => {
+    table.integer('social_media_id').unsigned();
+    table.foreign('social_media_id').references('social_media');
+  }),
+]);
+
+
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.dropTable('social_media'),
+  knex.schema.table('location', (table) => {
+    table.dropColumn('social_media_id');
+  }),
+]);
