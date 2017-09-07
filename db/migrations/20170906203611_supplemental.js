999f2508900c3b0d6c1052361f4e61ed0da1bf8d
coleworsley <cole.s.worsley@gmail.com>
2017-09-06 20:41:01 -0600

    add tables for status type and location type

---
+++ db/migrations/20170906203611_supplemental.js
@@ -0,0 +1,36 @@
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTable('location_type', (table) => {
+    table.increments('id').primary();
+    table.string('type');
+  }),
+
+  knex.schema.createTable('status_type', (table) => {
+    table.increments('id').primary();
+    table.string('type');
+  }),
+
+  knex.schema.table('location', (table) => {
+    table.integer('location_type_id').unsigned();
+    table.foreign('location_type_id').references('location_type.id');
+  }),
+
+  knex.schema.table('happy_hour', (table) => {
+    table.integer('status_type_id').unsigned();
+    table.foreign('status_type_id').references('status_type.id');
+  }),
+]);
+
+
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.table('happy_hour', (table) => {
+    table.dropColumn('status_type_id');
+  }),
+
+  knex.schema.table('location', (table) => {
+    table.dropColumn('location_type_id');
+  }),
+
+  knex.schema.dropTable('status_type'),
+  
+  knex.schema.dropTable('location_type'),
+]);
