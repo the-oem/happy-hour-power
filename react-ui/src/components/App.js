32b4760aea45f405982d055b5e1c704f27c7ee5e
coleworsley <cole.s.worsley@gmail.com>
2017-09-08 13:07:29 -0600

    add map to DOM

--- react-ui/src/components/App.js
+++ react-ui/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../styles/App.css';
+import { Map } from './Map';
 
 class App extends Component {
   constructor(props) {
@@ -29,7 +30,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1>Happy Hour Power</h1>
-
+          <Map
+            containerElement={<div className="map-container"></div>}
+            mapElement={<div className="map-element"></div>}
+          />
         </div>
       </div>
     );
