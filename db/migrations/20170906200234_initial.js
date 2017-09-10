a85986b59f25f69526fa7178870248bf4c401af0
Jason Collins <the.oem@gmail.com>
2017-09-10 12:57:40 -0600

    Update object model to be plural for locations and happy hours.

--- db/migrations/20170906200234_initial.js
+++ db/migrations/20170906200234_initial.js
@@ -1,6 +1,6 @@
 
 exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTable('location', (table) => {
+  knex.schema.createTable('locations', (table) => {
     table.increments('id').primary();
     table.string('name');
     table.float('latitude');
@@ -10,20 +10,20 @@ exports.up = (knex, Promise) => Promise.all([
     table.timestamps(true, true);
   }),
 
-  knex.schema.createTable('happy_hour', (table) => {
+  knex.schema.createTable('happy_hours', (table) => {
     table.increments('id').primary();
     table.string('timeslot');
     table.text('drink_specials', 'longtext');
     table.text('food_specials', 'longtext');
     table.binary('menu_pictures');
     table.integer('location_id').unsigned();
-    table.foreign('location_id').references('location.id');
+    table.foreign('location_id').references('locations.id');
   }),
 
 ]);
 
 
 exports.down = (knex, Promise) => Promise.all([
-  knex.schema.dropTable('happy_hour'),
-  knex.schema.dropTable('location'),
+  knex.schema.dropTable('happy_hours'),
+  knex.schema.dropTable('locations'),
 ]);
--- db/migrations/20170906203611_supplemental.js
+++ db/migrations/20170906203611_supplemental.js
@@ -9,12 +9,12 @@ exports.up = (knex, Promise) => Promise.all([
     table.string('type');
   }),
 
-  knex.schema.table('location', (table) => {
+  knex.schema.table('locations', (table) => {
     table.integer('location_type_id').unsigned();
     table.foreign('location_type_id').references('location_type.id');
   }),
 
-  knex.schema.table('happy_hour', (table) => {
+  knex.schema.table('happy_hours', (table) => {
     table.integer('status_type_id').unsigned();
     table.foreign('status_type_id').references('status_type.id');
   }),
@@ -22,15 +22,14 @@ exports.up = (knex, Promise) => Promise.all([
 
 
 exports.down = (knex, Promise) => Promise.all([
-  knex.schema.table('happy_hour', (table) => {
+  knex.schema.table('happy_hours', (table) => {
     table.dropColumn('status_type_id');
   }),
 
-  knex.schema.table('location', (table) => {
+  knex.schema.table('locations', (table) => {
     table.dropColumn('location_type_id');
   }),
 
   knex.schema.dropTable('status_type'),
-  
   knex.schema.dropTable('location_type'),
 ]);
