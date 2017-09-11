8c7fa31a07c6b30241cec10b5fb71484998d9a8e
Daniel Bucket <danieljbucket@gmail.com>
2017-09-10 22:36:08 -0600

    DELETE all data pertaining to a location by id

--- server/index.js
+++ server/index.js
@@ -24,26 +24,25 @@ app.set('secretKey', 'FAKE-process.env.SECRET_KEY');
 // All remaining requests return the React app, so it can handle routing.
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-});
+})
 
 app.post('/api/v1/admin/', (req, res) => {
 	const payload = req.body;
 
-	for (let requiredParams of ['userType', "email"]) {
+	for (let requiredParams of ['businessID', "email"]) {
 		if (!req.body[requiredParams]) {
 			return res.status.json({ error: `Mising requried parameter "${requiredParams}"`})
 		}
 	}
 
 	if (payload.email.endsWith('@controllerAdmin.com')) {
-		Object.assign(payload, { admin:true })
+		Object.assign(payload, { admin:true }, { businessID:req.body.businessID })
 	} else { Object.assign(payload, { admin:false }) }
 
 	const token = jwt.sign(payload, app.get('secretKey'), {expiresIn: '7d'})
 	return res.status(200).json({ token })
 })
 
-// GET ALL LOCATIONS
 app.route('/api/v1/location')
 .get((req, res) => {
 	db('location').select()
@@ -71,37 +70,68 @@ app.route('/api/v1/location')
 db('location_type').insert(locationType, 'id')
 .then(locTypeID => {
 		Object.assign(newLocation, { location_type_id:locTypeID[0] })
-
 		db('social_media').insert(newSocialMedia, 'id')
 		.then(socialMediaID => {
 			Object.assign(newLocation, { social_media_id:socialMediaID[0] })
-
 			db('location').insert(newLocation, ['id'])
 			.then(newLocID => {
-
 				db('status_type').insert(statusType, 'id')
 				.then(statusID => {
 					Object.assign(newHappyHour, { status_type_id:statusID[0] }, { location_id:newLocID[0].id })
-
 					db('happy_hour').insert(newHappyHour, ['location_id'])
 					.then(locID => {
-
 						db('location').where('id', locID[0].location_id).select()
 						.then(data => res.status(200).json({ data }))
 					})
-					.catch(error => res.status(200).json({ error }))
+					.catch(error => res.status(500).json({ error }))
 				})
-				.catch(error => res.status(200).json({ error }))
+				.catch(error => res.status(500).json({ error }))
 			})
-			.catch(error => res.status(200).json({ error }))
+			.catch(error => res.status(500).json({ error }))
 		})
 		.catch(error => res.status(500).json({ error }))
 	})
 	.catch(error => res.status(500).json({ error }))
 })
 
+
+
+app.delete('/api/v1/location/destroy/', checkAuth, (req, res) => {
+	const id = req.headers.businessID;
+	const businessName = req.body.businessName;
+
+		if (req.headers.statusType !== 'controller') {
+			res.status(500).json({
+				message:"You are not qualified to remove this business from the database."
+			})
+		}
+
+	db('happy_hour').where("id", id).del()
+	.then(id => {
+		db('location').where('id', id).del()
+		.then(id => {
+			db('social_media').where('id', id).del()
+			.then(id => {
+				db('location_type').where('id', id).del()
+				.then(id => {
+					db('status_type').where('id', id).del()
+					.then(id => {
+						res.status(200).json({ message:`All data pertaining to ${businessName} has been permanantly destroyed.` })
+					})
+					.catch(error => res.status(500).json({ error }))
+				})
+				.catch(error => res.status(500).json({ error }))
+			})
+			.catch(error => res.status(500).json({ error }))
+		})
+		.catch(error => res.status(500).json({ error }))
+	})
+	.catch(error => res.status(500).json({ error }))
+})
+
+
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${ PORT }`);
 });
 
 module.exports = app;
--- server/serverMiddleware.js
+++ server/serverMiddleware.js
@@ -5,7 +5,7 @@ const checkAuth = (req, res, next) => {
 
   if (!tokenPiece) {
     return res.status(403).json({
-      message: 'You must have be authorized to do this action. Contact your computer guy',
+      message: 'You must have be authorized to proceed. Contact your nearest computer guy',
     });
   }
 
@@ -13,23 +13,21 @@ const checkAuth = (req, res, next) => {
   jwt.verify(tokenPiece, 'FAKE-process.env.SECRET_KEY', (error, decoded) => {
     if (error) {
       return res.status(403).json({
-        message: 'Gandalf says you shall not pass',
-        error,
+        message: 'Gandalf says you shall not pass.', error
       })
     }
 
     if (decoded.admin) {
-      Object.assign(decoded, { statusType:'controller' })
+      Object.assign(req.headers, { statusType:'controller' }, { businessID:decoded.businessID })
       next()
     } else {
-      Object.assign(decoded, { statusType:"user" })
+      Object.assign(req.headers, { statusType:"user" })
       next()
     }
     return null
   })
   return null
 }
-
 
 module.exports = {
   checkAuth
