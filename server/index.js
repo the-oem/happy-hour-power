07ffc324c3fca581e2e7de9f1b75d97f4cc9bcd3
Daniel Bucket <danieljbucket@gmail.com>
2017-09-11 08:53:49 -0600

    put route for location_type

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const { checkAuth,
 																												require('dotenv').config();
 
 const PORT 															= process.env.PORT || 5000;
-const environment 								= process.env.NODE_ENV || 'development'
-const configuration 						= require('../knexfile')[environment]
-const db 																	= require('knex')(configuration)
+const environment 								= process.env.NODE_ENV || 'development';
+const configuration 						= require('../knexfile')[environment];
+const db 																	= require('knex')(configuration);
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
@@ -18,8 +18,6 @@ app.use(bodyParser.json());
 
 // app.set('secretKey', process.env.SECRET_KEY);
 app.set('secretKey', 'FAKE-process.env.SECRET_KEY');
-
-
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('/', (req, res) => {
@@ -144,11 +142,11 @@ app.delete('/api/v1/location/destroy/', checkAuth, (req, res) => {
 	const id 										= req.headers.businessID;
 	const businessName = req.body.businessName;
 
-		if (req.headers.statusType !== 'controller') {
-			res.status(500).json({
-				message:"You are not qualified to remove this business from the database."
-			})
-		}
+	if (req.headers.statusType !== 'controller') {
+		res.status(401).json({
+			message:"You are not qualified to remove this business from the database."
+		})
+	}
 
 	db('happy_hour').where("id", id).del()
 	.then(id => {
@@ -171,9 +169,26 @@ app.delete('/api/v1/location/destroy/', checkAuth, (req, res) => {
 		.catch(error => res.status(500).json({ error }))
 	})
 	.catch(error => res.status(500).json({ error }))
+});
+
+app.route('/api/v1/locationtype/update/')
+.put(checkAuth, (req, res) => {
+	const id 													= req.headers.businessID
+	const newLocationType = req.body.locationType;
+
+	if (req.headers.statusType !== 'controller') {
+		res.status(401).json({
+			message:'you are not qualified to edit this modify this business'
+		})
+	}
+
+	db('location_type').where('id', id).select()
+	.update(newLocationType, '*')
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`Listening on port ${ PORT }`);
 });
 
