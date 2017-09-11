d5bb8deb10ef8429499f08a9c1005dcea1108177
Jason Collins <the.oem@gmail.com>
2017-09-10 22:54:51 -0600

    Add status_type seed file.

---
+++ db/seeds/dev/status_types.js
@@ -0,0 +1,25 @@
+const status_types = [
+	{
+		"id": 1,
+		"type": "establishment_approved"
+	},
+	{
+		"id": 2,
+    "type": "user_submitted"
+	},
+  {
+    "id": 3,
+    "type": "crowdsourced_approved"
+  },
+]
+
+exports.seed = (knex, Promise) => {
+	return knex('status_type').del()
+			.then(() => {
+				return Promise.all(
+					status_types.map(type => {
+						return knex('status_type').insert(type);
+					}))
+			})
+			.catch((error) => console.log({ error }))
+}
