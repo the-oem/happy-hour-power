eaa3ce8d828bc517fdad81c25f5aede8a1279b12
coleworsley <cole.s.worsley@gmail.com>
2017-09-11 16:02:30 -0600

    add geolocation action

---
+++ react-ui/src/actions/maps.js
@@ -0,0 +1,24 @@
+import { DEFAULT_LOCATION } from '../utils/constants';
+
+export const updateLocation = (location) => {
+  return {
+    type: 'GEOLOCATE',
+    location,
+  }
+}
+
+export const geolocate = () => {
+  return (dispatch) => {
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        dispatch(updateLocation({ latitude, longitude }));
+      });
+    } else {
+
+      dispatch(updateLocation(DEFAULT_LOCATION.coordinates))
+    }
+  }
+}
