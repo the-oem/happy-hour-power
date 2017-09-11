465c33c2cf0eb1e5fadff9643eaa8aadee786e20
Jason Collins <the.oem@gmail.com>
2017-09-10 22:24:45 -0600

    Add seed file to dev. Clean up format to fix errors when trying to seed data.

--- db/migrations/20170906204329_settings.js
+++ db/migrations/20170906204329_settings.js
@@ -1,11 +1,9 @@
-
 exports.up = (knex, Promise) => Promise.all([
   knex.schema.createTable('settings', (table) => {
     table.increments('id').primary();
     table.float('default_search_radius');
   }),
 ]);
-
 
 exports.down = (knex, Promise) => Promise.all([
   knex.schema.dropTable('settings'),
