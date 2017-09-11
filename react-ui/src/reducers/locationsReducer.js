bb0541f380064e01751764f3fb80aae4d8b252a0
coleworsley <cole.s.worsley@gmail.com>
2017-09-11 13:37:12 -0600

    add locations reducer

---
+++ react-ui/src/reducers/locationsReducer.js
@@ -0,0 +1,10 @@
+const locationsReducer = (state=[], action) => {
+  switch (expression) {
+    case 'LOCATION_EXAMPLE':
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default locationsReducer;
