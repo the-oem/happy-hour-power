8aa093b7893c380b0d5f6212cd0d9fe93026c0bb
Justyna Field <justyna.field@gmail.com>
2017-09-12 13:53:36 -0600

    Reder addresses

--- react-ui/src/components/List.js
+++ react-ui/src/components/List.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 export class List extends Component {
   render() {
     const displayLocation = this.props.locations.map(location => (
-      <Location {...location} />
+      <Location key={location.id} {...location} />
+      // console.log('location: ', location)
     ));
 
     return <div>{displayLocation}</div>;
   }
 }
 
-const Location = ({ name }) => {
-  return <div>{name}</div>;
+const Location = ({ name, vicinity }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Address: {vicinity}</p>
+    </div>
+  );
 };
