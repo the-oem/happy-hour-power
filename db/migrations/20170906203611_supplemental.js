292244de39718bd35acc91456e6fd79e565b5e6d
Jason Collins <the.oem@gmail.com>
2017-09-11 20:01:30 -0600

    Pretty the DB files.

--- db/migrations/20170906203611_supplemental.js
+++ db/migrations/20170906203611_supplemental.js
@@ -1,35 +1,36 @@
-exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTable('location_type', (table) => {
-    table.increments('id').primary();
-    table.string('type');
-  }),
+exports.up = (knex, Promise) =>
+  Promise.all([
+    knex.schema.createTable('location_type', table => {
+      table.increments('id').primary();
+      table.string('type');
+    }),
 
-  knex.schema.createTable('status_type', (table) => {
-    table.increments('id').primary();
-    table.string('type');
-  }),
+    knex.schema.createTable('status_type', table => {
+      table.increments('id').primary();
+      table.string('type');
+    }),
 
-  knex.schema.table('locations', (table) => {
-    table.integer('location_type_id').unsigned();
-    table.foreign('location_type_id').references('location_type.id');
-  }),
+    knex.schema.table('locations', table => {
+      table.integer('location_type_id').unsigned();
+      table.foreign('location_type_id').references('location_type.id');
+    }),
 
-  knex.schema.table('happy_hours', (table) => {
-    table.integer('status_type_id').unsigned();
-    table.foreign('status_type_id').references('status_type.id');
-  }),
-]);
+    knex.schema.table('happy_hours', table => {
+      table.integer('status_type_id').unsigned();
+      table.foreign('status_type_id').references('status_type.id');
+    })
+  ]);
 
+exports.down = (knex, Promise) =>
+  Promise.all([
+    knex.schema.table('happy_hours', table => {
+      table.dropColumn('status_type_id');
+    }),
 
-exports.down = (knex, Promise) => Promise.all([
-  knex.schema.table('happy_hours', (table) => {
-    table.dropColumn('status_type_id');
-  }),
+    knex.schema.table('locations', table => {
+      table.dropColumn('location_type_id');
+    }),
 
-  knex.schema.table('locations', (table) => {
-    table.dropColumn('location_type_id');
-  }),
-
-  knex.schema.dropTable('status_type'),
-  knex.schema.dropTable('location_type'),
-]);
+    knex.schema.dropTable('status_type'),
+    knex.schema.dropTable('location_type')
+  ]);
