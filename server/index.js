3855a76ba4172bfe3d37aae7841e98dae69309cb
Daniel Bucket <danieljbucket@gmail.com>
2017-09-12 00:00:12 -0600

    location_type: PATCH, GET, GET all; social_media: PATCH, GET, GET all;

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const 		express 											= require('express');
-const 		app 															= express();
-const 		path 														= require('path');
-const 		bodyParser 								= require('body-parser');
-const 		jwt 															= require('jsonwebtoken');
+const 		express 													= require('express');
+const 		app 																	= express();
+const 		path 																= require('path');
+const 		bodyParser 										= require('body-parser');
+const 		jwt 																	= require('jsonwebtoken');
 const { checkAuth,
-								happyHourParams		} = require('./serverMiddleware');
-																													require('dotenv').config();
+								happyHourParams,
+								socialMediaParams,
+								patchSocialMedia		} 	= require('./serverMiddleware');
+																															require('dotenv').config();
 
-const PORT 																= process.env.PORT || 5000;
-const environment 									= process.env.NODE_ENV || 'development';
-const configuration 							= require('../knexfile')[environment];
-const db 																		= require('knex')(configuration);
+const PORT 																		= process.env.PORT || 5000;
+const environment 											= process.env.NODE_ENV || 'development';
+const configuration 									= require('../knexfile')[environment];
+const db 																				= require('knex')(configuration);
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
@@ -116,7 +118,7 @@ app.route('/api/v1/happyhour/update')
 	for (let requiredParams of ['timeslot','drink_specials','food_specials','menu_pictures']) {
 		if (!newHappyHourData[requiredParams]) {
 			return res.status(422).json({
-				error: `Missing required parameter ${requiredParams}`
+				error: `Missing required parameter '${requiredParams}'`
 			})
 		}
 	}
@@ -199,10 +201,14 @@ app.get('/api/v1/locationtype/:type', (req,res) => {
 	const newType = req.params.type;
 
 	db('location_type').where('type', newType).select('*')
-	.then(data => {
-		res.status(200).json({ data })
-	})
+	.then(data => res.status(200).json({ data }))
 	.catch(error => res.status(400).json({ error }))
+})
+
+app.get('/api/v1/locationtype', (req, res) => {
+	db('location_type').select()
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
 app.route('/api/v1/statustype/update/')
@@ -232,13 +238,42 @@ app.route('/api/v1/statustype/update/')
 
 app.get('/api/v1/statustype/:type', (req, res) => {
 	const newType = req.params.type;
+
 	db('status_type').where('type', newType).select('id')
 	.then(data => res.status(200).json({ data }))
 })
 
 app.get('/api/v1/statustype', (req, res) => {
+
 	db('status_type').select('*')
 	.then(data =>  res.status(200).json({ data }))
+})
+
+app.route('/api/v1/socialmedia/')
+.get((req, res) => {
+
+	db('social_media').select()
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
+})
+.patch(checkAuth, patchSocialMedia, (req, res) => {
+		const id 						= req.headers.businessID;
+		const newMedia = req.body;
+
+		db('social_media').where('id', id)
+		.select(`${newMedia}`)
+		.update(newMedia, '*')
+		.then(updatedMedia => res.status(200).json({ updatedMedia }))
+		.catch(error => res.status(500).json({ error }))
+})
+
+app.route('/api/v1/socialmedia/:type')
+.get(socialMediaParams, (req, res) => {
+	const newType = req.params.type;
+
+	db('social_media').select(newType, 'id')
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
 app.listen(PORT, () => {
--- server/serverMiddleware.js
+++ server/serverMiddleware.js
@@ -40,7 +40,33 @@ const happyHourParams = (req, res, next) => {
   }
 }
 
+const socialMediaParams = (req, res, next) => {
+  const searchParam = req.params.type;
+  const paramsOptions = ['yelp', 'instagram', 'twitter', 'pinterest', 'facebook'];
+
+  if (paramsOptions.indexOf(searchParam) === -1) {
+    res.status(422).json({
+      error: `The parameter '${searchParam}' is not a value in the database`
+    })
+  } else {
+    next()
+  }
+}
+
+const patchSocialMedia = (req, res, next) => {
+  const paramsOptions = ['yelp', 'instagram', 'twitter', 'pinterest', 'facebook'];
+  const newParamKey   = Object.keys(req.body)[0];
+
+    if (paramsOptions.indexOf(newParamKey) === -1) {
+    return res.status(422).json({ message:`${newParamKey} is not a valid key` })
+  } else {
+    next()
+  }
+}
+
 module.exports = {
   checkAuth,
-  happyHourParams
+  happyHourParams,
+  socialMediaParams,
+  patchSocialMedia
 };
