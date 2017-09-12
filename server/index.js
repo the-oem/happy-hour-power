2d3bd062cf4b8250c87a258ad714d669a58d7278
Daniel Bucket <danieljbucket@gmail.com>
2017-09-12 00:00:12 -0600

    location_type: PATCH, GET, GET all; social_media: PATCH, GET, GET all;

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-const 		express 											= require('express');
-const 		app 															= express();
-const 		path 														= require('path');
-const 		bodyParser 								= require('body-parser');
-const 		jwt 															= require('jsonwebtoken');
+const 		express 													= require('express');
+const 		app 																	= express();
+const 		path 																= require('path');
+const 		bodyParser 										= require('body-parser');
+const 		jwt 																	= require('jsonwebtoken');
 const { checkAuth,
 								happyHourParams,
-								statusTypeParams } = require('./serverMiddleware');
-																													require('dotenv').config();
+								socialMediaParams,
+								patchSocialMedia		} 	= require('./serverMiddleware');
+																															require('dotenv').config();
 
-const PORT 																= process.env.PORT || 5000;
-const environment 									= process.env.NODE_ENV || 'development';
-const configuration 							= require('../knexfile')[environment];
-const db 																		= require('knex')(configuration);
+const PORT 																		= process.env.PORT || 5000;
+const environment 											= process.env.NODE_ENV || 'development';
+const configuration 									= require('../knexfile')[environment];
+const db 																				= require('knex')(configuration);
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
@@ -117,7 +118,7 @@ app.route('/api/v1/happyhour/update')
 	for (let requiredParams of ['timeslot','drink_specials','food_specials','menu_pictures']) {
 		if (!newHappyHourData[requiredParams]) {
 			return res.status(422).json({
-				error: `Missing required parameter ${requiredParams}`
+				error: `Missing required parameter '${requiredParams}'`
 			})
 		}
 	}
@@ -201,10 +202,14 @@ app.get('/api/v1/locationtype/:type', (req,res) => {
 
 
 	db('location_type').where('type', newType).select('*')
-	.then(data => {
-		res.status(200).json({ data })
-	})
+	.then(data => res.status(200).json({ data }))
 	.catch(error => res.status(400).json({ error }))
+})
+
+app.get('/api/v1/locationtype', (req, res) => {
+	db('location_type').select()
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
 app.route('/api/v1/statustype/update/')
@@ -240,11 +245,39 @@ app.get('/api/v1/statustype/:type', (req, res) => {
 	.catch(error => res.status(400).json({ error }))
 })
 
-app.get('/api/v1/statustype', (req,res) => {
+app.get('/api/v1/statustype', (req, res) => {
+
 	db('status_type').select('*')
 	.then(data => {
 		res.status(200).json({ data })
 	})
+})
+
+app.route('/api/v1/socialmedia/')
+.get((req, res) => {
+
+	db('social_media').select()
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
+})
+.patch(checkAuth, patchSocialMedia, (req, res) => {
+		const id 						= req.headers.businessID;
+		const newMedia = req.body;
+
+		db('social_media').where('id', id)
+		.select(`${newMedia}`)
+		.update(newMedia, '*')
+		.then(updatedMedia => res.status(200).json({ updatedMedia }))
+		.catch(error => res.status(500).json({ error }))
+})
+
+app.route('/api/v1/socialmedia/:type')
+.get(socialMediaParams, (req, res) => {
+	const newType = req.params.type;
+
+	db('social_media').select(newType, 'id')
+	.then(data => res.status(200).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
 app.listen(PORT, () => {
