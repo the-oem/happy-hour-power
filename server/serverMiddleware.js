33662e24993356676f6c9afa1265c5e3ec27ed61
Daniel Bucket <danieljbucket@gmail.com>
2017-09-11 19:07:25 -0600

    GET for status type by type and GET all

--- server/serverMiddleware.js
+++ server/serverMiddleware.js
@@ -31,10 +31,10 @@ const checkAuth = (req, res, next) => {
 
 const happyHourParams = (req, res, next) => {
   const paramsOptions = ['timeslot','drink_specials','food_specials','menu_pictures'];
-  const newParamKey = Object.keys(req.body)[0];
+  const newParamKey   = Object.keys(req.body)[0];
 
   if (paramsOptions.indexOf(newParamKey) === -1) {
-    res.status(422).json({ message:`${newParamKey} is not a valid key` })
+    return res.status(422).json({ message:`${newParamKey} is not a valid key` })
   } else {
     next()
   }
