25918b202368e5a618152c5127b8a03d8cf42447
coleworsley <cole.s.worsley@gmail.com>
2017-09-12 12:15:43 -0600

    remove unneccesary code

--- react-ui/src/components/RenderMap.js
+++ react-ui/src/components/RenderMap.js
@@ -1,49 +1,13 @@
 import React, { Component } from 'react';
-// import '../styles/App.css';
 import MapContainer from '../containers/MapContainer';
-import { Map } from './Map';
 
 export default class RenderMap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      markers: [
-        {
-          position: {
-            lat: 39.75084,
-            lng: -104.996529
-          },
-          defaultAnimation: 2
-        }
-      ]
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        return response.json();
-      })
-      .then(json => {
-        console.log('/api data: ', json);
-      })
-      .catch(e => {
-        console.log('error: ', e);
-      });
-  }
-
   render() {
     return (
       <div className="map-area">
-        <MapContainer
-          map={this.props.map}
-          containerElement={<div className="map-container" />}
-          mapElement={<div className="map-element" />}
-          onMapLoad={this.handleMapLoad}
-          onMapClick={this.handleMapClick}
-          markers={this.state.markers}
-          onMarkerRightClick={this.handleMarkerRightClick}
-        />
+          <MapContainer
+            map={this.props.map}
+          />
       </div>
     );
   }
