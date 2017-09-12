6136071d7acfa6aceb044f9ee5bba1549f4d55ef
Jason Collins <the.oem@gmail.com>
2017-09-11 18:10:07 -0600

    Fix breaking tests and get the app back to it's initial testing configuration.

--- react-ui/src/tests/actionTests/actions.test.js
+++ react-ui/src/tests/actionTests/actions.test.js
@@ -2,6 +2,6 @@ import * as actions from '../../actions'
 
 describe('action tests', () => {
   it('should create an action to render map', () => {
-    // expect(true).toEqual(true)
+    expect(true).toEqual(true)
   })
 })
