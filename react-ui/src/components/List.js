fcda841aac3eb27dfcf2928f220e3b3ee37b8c1c
Justyna Field <justyna.field@gmail.com>
2017-09-12 15:19:43 -0600

    Clean code

--- react-ui/src/components/List.js
+++ react-ui/src/components/List.js
@@ -5,7 +5,6 @@ export class List extends Component {
   render() {
     const displayLocation = this.props.locations.map(location => (
       <Location key={location.id} {...location} />
-      // console.log('location: ', location)
     ));
 
     return <div>{displayLocation}</div>;
