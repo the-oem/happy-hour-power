348044a175aed4e977870869e53100768545a539
Jason Collins <the.oem@gmail.com>
2017-09-13 16:25:43 -0600

    Add base test setup for happy hours endpoints.

--- server/tests/happyhour.routes.test.js
+++ server/tests/happyhour.routes.test.js
@@ -1,16 +1,29 @@
+require('dotenv').config();
+
 const chai = require('chai');
-
 const should = chai.should();
-const chaiHttp = require('chai-http');
-
+const chaiHTTP = require('chai-http');
 const server = require('../index.js');
 
-chai.use(chaiHttp);
+const environment = process.env.NODE_ENV;
+const configuration = require('../../knexfile.js')[environment];
+const db = require('knex')(configuration);
+
+chai.use(chaiHTTP);
 
 describe('Testing HappyHour API Routes', () => {
+  beforeEach(done => {
+    db.migrate.rollback().then(() => {
+      db.migrate.latest().then(() => {
+        db.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+  });
 
-  describe('GET /api', () => {
-    it('should respond with a JSON response', (done) => {
+  describe('GET /api/v1/happyhours', () => {
+    it('does a thing', done => {
       done();
     });
   });
