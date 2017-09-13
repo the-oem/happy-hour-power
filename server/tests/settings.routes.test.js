5c6c873381281c3d0fbccd06f89f525da89f92f0
Jason Collins <the.oem@gmail.com>
2017-09-13 16:27:09 -0600

    Add base test setup for settings endpoints.

---
+++ server/tests/settings.routes.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHTTP = require('chai-http');
+const server = require('../index.js');
+
+const environment = process.env.NODE_ENV;
+const configuration = require('../../knexfile.js')[environment];
+const db = require('knex')(configuration);
+
+chai.use(chaiHTTP);
+
+describe('Testing Settings API Routes', () => {
+  beforeEach(done => {
+    db.migrate.rollback().then(() => {
+      db.migrate.latest().then(() => {
+        db.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET /api/v1/settings', () => {
+    it('does a thing', done => {
+      done();
+    });
+  });
+});
