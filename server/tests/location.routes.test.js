4ca28159fd918c3578106c80e812be592f7d39c9
Jason Collins <the.oem@gmail.com>
2017-09-16 08:48:37 -0600

    Add GET locations test.

--- server/tests/location.routes.test.js
+++ server/tests/location.routes.test.js
@@ -23,8 +23,86 @@ describe('Testing Location API Routes', () => {
   });
 
   describe('GET /api/v1/locations', () => {
-    it('does a thing', done => {
-      done();
+    it('should respond with a 200 response and all locations', done => {
+      chai
+        .request(server)
+        .get('/api/v1/locations')
+        .end((err, res) => {
+          should.not.exist(err);
+          res.status.should.equal(200);
+          res.type.should.equal('application/json');
+          res.body.data[0].should.include.keys(
+            'id',
+            'name',
+            'latitude',
+            'longitude',
+            'phone_number',
+            'website_url',
+            'google_maps_id',
+            'created_at',
+            'updated_at',
+            'location_type_id'
+          );
+          done();
+        });
     });
+
+    it.skip(
+      'should respond with a 200 response and all locations, filtered by a query param',
+      done => {
+        chai
+          .request(server)
+          .get('/api/v1/items?cleanliness=Dusty')
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.equal(200);
+            res.type.should.equal('application/json');
+            res.body.data.length.should.equal(3);
+            res.body.data[0].should.include.keys(
+              'id',
+              'name',
+              'staleness_reason',
+              'cleanliness',
+              'created_at',
+              'updated_at'
+            );
+            done();
+          });
+      }
+    );
+
+    it.skip(
+      'should respond with a 200 response and an empty array if no filtered items are found',
+      done => {
+        chai
+          .request(server)
+          .get('/api/v1/items?cleanliness=Clean')
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.equal(200);
+            res.type.should.equal('application/json');
+            res.body.data.length.should.equal(0);
+
+            done();
+          });
+      }
+    );
+
+    it.skip(
+      'should respond with a 500 error if a filter query param is misspelled',
+      done => {
+        chai
+          .request(server)
+          .get('/api/v1/items?cleaniness=Dusty')
+          .end((err, res) => {
+            should.exist(err);
+            res.status.should.equal(500);
+            res.type.should.equal('application/json');
+            res.body.error.severity.should.equal('ERROR');
+            res.body.error.routine.should.equal('errorMissingColumn');
+            done();
+          });
+      }
+    );
   });
 });
