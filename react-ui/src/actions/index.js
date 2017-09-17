9b2c0ac068ec80b4f29bdba43f7c922d86ba7782
Justyna Field <justyna.field@gmail.com>
2017-09-17 13:21:01 -0600

    Add test for locations action, skipped test that were not finished

--- react-ui/src/actions/index.js
+++ react-ui/src/actions/index.js
@@ -1,10 +1,3 @@
 export * from './googleMaps';
 export * from './locations';
 export * from './markers';
-
-// export const toggleMapView = view => {
-//   return {
-//     type: 'TOGGLE_VIEW',
-//     view
-//   };
-// };
--- react-ui/src/tests/actionTests/googleMaps.test.js
+++ react-ui/src/tests/actionTests/googleMaps.test.js
@@ -1,17 +1,16 @@
 import * as actions from '../../actions';
 
 describe('googleMaps actions', () => {
-  // it.skip('should create an action to find current location', () => {
-  //
-  //   const location = {
-  //     location:
-  //   }
-  //   const expectedAction = {
-  //     type: 'CURRENT_LOCATION',
-  //     location
-  //   }
-  //   expect(actions.currentLocation(location)).toEqual(expectedAction)
-  // });
+  it.skip('should create an action to find current location', () => {
+    const location = {
+      location
+    };
+    const expectedAction = {
+      type: 'CURRENT_LOCATION',
+      location
+    };
+    expect(actions.currentLocation(location)).toEqual(expectedAction);
+  });
 
   it('should create an action to find nearby locations', () => {
     const data = [
@@ -41,15 +40,14 @@ describe('googleMaps actions', () => {
     expect(actions.nearbyLocations(data)).toEqual(expectedAction);
   });
 
-  // it.skip('should create an action to find map center', () => {
-  //
-  //   const position = {
-  //     position:
-  //   }
-  //   const expectedAction = {
-  //     type: 'CENTER',
-  //     position
-  //   }
-  //   expect(actions.mapCenter(position)).toEqual(expectedAction)
-  // });
+  it.skip('should create an action to find map center', () => {
+    const position = {
+      position
+    };
+    const expectedAction = {
+      type: 'CENTER',
+      position
+    };
+    expect(actions.mapCenter(position)).toEqual(expectedAction);
+  });
 });
