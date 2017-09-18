7238b20f16416bf0abc031c88ea8057f7c3774b7
coleworsley <cole.s.worsley@gmail.com>
2017-09-18 16:02:10 -0600

    adjust footer to pop out when data a marker is selected

--- react-ui/src/components/LocationDetails.js
+++ react-ui/src/components/LocationDetails.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import { DisplayLocationDetails } from './DisplayLocationDetails';
 import '../styles/LocationDetails.css';
 
 export default class LocationDetails extends Component {
-  constructor(props) {
+  constructor() {
     super();
+    this.state = {
+      hidden: true
+    };
+    this.toggleHidden = this.toggleHidden.bind(this);
   }
 
-  renderDetails() {
-    return this.props.currentLocation && this.props.currentLocation.name ? (
-      <DisplayLocationDetails location={this.props.currentLocation} />
-    ) : (
-      <div className="display-none" />
-    );
+  toggleHidden() {
+    this.setState({ hidden: !this.state.hidden })
   }
 
   render() {
-    return <div className="current-location">{this.renderDetails()}</div>;
+    const { name, vicinity } = this.props.currentLocation;
+    const { hidden } = this.state;
+
+    // need to refactor
+    const status = name ? 'shown' : 'hidden';
+
+    return (
+      <div className={`current-location ${status}`}>
+        <h3 className='loc-name'>{name}</h3>
+        <p className='loc-details'>
+          <b>Address:</b> {vicinity}
+        </p>
+        <button
+          className="show-details"
+          onClick={this.showDetails}>
+
+        </button>
+        <button
+          className='current-location-toggle'
+          onClick={this.toggleHidden}
+          >
+          { hidden ? 'Show' : 'Hide' }
+        </button>
+      </div>
+    );
   }
 }
