1ec08b994af78952a42cfa68406e043692f9937a
Justyna Field <justyna.field@gmail.com>
2017-09-18 18:14:04 -0600

    Structure happy hours on DetailsPage

--- react-ui/src/components/DetailsPage.js
+++ react-ui/src/components/DetailsPage.js
@@ -8,10 +8,23 @@ export class DetailsPage extends Component {
   render() {
     const { name } = this.props.locationDetails.location;
     const happyHours = this.props.locationDetails.happyhours.map(hours => {
-      const { timeslot, drink_specials, food_specials, locations_id } = hours;
+      const { timeslot, drink_specials, food_specials, id } = hours;
+
+      const day = timeslot.slice(0, 3).toUpperCase();
+      const beginTime = timeslot.slice(4, 9);
+      const endTime = timeslot.slice(10, 15);
+      const happyHourTimes = (
+        <div>
+          <p>{day}</p>
+          <p>
+            {beginTime} - {endTime}
+          </p>
+        </div>
+      );
+
       return (
-        <div key={locations_id}>
-          <p>{timeslot}</p>
+        <div key={id}>
+          {happyHourTimes}
           <p>{drink_specials}</p>
           <p>{food_specials}</p>
         </div>
@@ -21,7 +34,6 @@ export class DetailsPage extends Component {
     return (
       <div>
         <h3>{name}</h3>
-
         {happyHours}
       </div>
     );
