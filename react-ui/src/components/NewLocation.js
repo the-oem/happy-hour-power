c8d30524f83671178524d67c4b6e26d644d39569
Justyna Field <justyna.field@gmail.com>
2017-09-19 19:32:27 -0600

    Style New Location Component

--- react-ui/src/components/NewLocation.js
+++ react-ui/src/components/NewLocation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/NewLocation.css';
 
 export class NewLocation extends Component {
   constructor(props) {
@@ -29,93 +30,116 @@ export class NewLocation extends Component {
     const { name, rating, vicinity } = this.props.activeLocation;
 
     return (
-      <div>
+      <div className="details-page-container">
         <h3>{name}</h3>
-        <p>There are no happy hours listed for this location.</p>
-        <p>Add Happy Hour Information</p>
+        <p className="info">
+          There are no happy hours listed for this location.
+        </p>
+        <p className="add">Add Happy Hour Information</p>
 
-        <label>Day</label>
-        <select onChange={e => this.updateInput(e, 'day')}>
-          <option>mon</option>
-          <option>tue</option>
-          <option>wed</option>
-          <option>thu</option>
-          <option>fri</option>
-          <option>sat</option>
-          <option>sun</option>
-        </select>
+        <div className="form">
+          <div className=" form-piece">
+            <p className="day-name">Day</p>
+            <select className="day" onChange={e => this.updateInput(e, 'day')}>
+              <option>mon</option>
+              <option>tue</option>
+              <option>wed</option>
+              <option>thu</option>
+              <option>fri</option>
+              <option>sat</option>
+              <option>sun</option>
+            </select>
+          </div>
 
-        <label>Start Time</label>
-        <select onChange={e => this.updateInput(e, 'startinghour')}>
-          <option>01:00</option>
-          <option>02:00</option>
-          <option>03:00</option>
-          <option>04:00</option>
-          <option>05:00</option>
-          <option>06:00</option>
-          <option>07:00</option>
-          <option>08:00</option>
-          <option>09:00</option>
-          <option>10:00</option>
-          <option>11:00</option>
-          <option>12:00</option>
-          <option>13:00</option>
-          <option>14:00</option>
-          <option>15:00</option>
-          <option>16:00</option>
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
-          <option>23:00</option>
-          <option>24:00</option>
-        </select>
+          <div className="times form-piece">
+            <label>Start Time</label>
+            <select
+              className="start"
+              onChange={e => this.updateInput(e, 'startinghour')}
+            >
+              <option>01:00</option>
+              <option>02:00</option>
+              <option>03:00</option>
+              <option>04:00</option>
+              <option>05:00</option>
+              <option>06:00</option>
+              <option>07:00</option>
+              <option>08:00</option>
+              <option>09:00</option>
+              <option>10:00</option>
+              <option>11:00</option>
+              <option>12:00</option>
+              <option>13:00</option>
+              <option>14:00</option>
+              <option>15:00</option>
+              <option>16:00</option>
+              <option>17:00</option>
+              <option>18:00</option>
+              <option>19:00</option>
+              <option>20:00</option>
+              <option>21:00</option>
+              <option>22:00</option>
+              <option>23:00</option>
+              <option>24:00</option>
+            </select>
 
-        <label>End Time</label>
-        <select onChange={e => this.updateInput(e, 'endingHour')}>
-          <option>01:00</option>
-          <option>02:00</option>
-          <option>03:00</option>
-          <option>04:00</option>
-          <option>05:00</option>
-          <option>06:00</option>
-          <option>07:00</option>
-          <option>08:00</option>
-          <option>09:00</option>
-          <option>10:00</option>
-          <option>11:00</option>
-          <option>12:00</option>
-          <option>13:00</option>
-          <option>14:00</option>
-          <option>15:00</option>
-          <option>16:00</option>
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
-          <option>23:00</option>
-          <option>24:00</option>
-        </select>
+            <label>End Time</label>
+            <select
+              className="end"
+              onChange={e => this.updateInput(e, 'endingHour')}
+            >
+              <option>01:00</option>
+              <option>02:00</option>
+              <option>03:00</option>
+              <option>04:00</option>
+              <option>05:00</option>
+              <option>06:00</option>
+              <option>07:00</option>
+              <option>08:00</option>
+              <option>09:00</option>
+              <option>10:00</option>
+              <option>11:00</option>
+              <option>12:00</option>
+              <option>13:00</option>
+              <option>14:00</option>
+              <option>15:00</option>
+              <option>16:00</option>
+              <option>17:00</option>
+              <option>18:00</option>
+              <option>19:00</option>
+              <option>20:00</option>
+              <option>21:00</option>
+              <option>22:00</option>
+              <option>23:00</option>
+              <option>24:00</option>
+            </select>
+          </div>
 
-        <p>Drink Specials</p>
-        <textarea
-          type="text"
-          value={this.state.drinkSpecials}
-          placeholder="Describe Drink Specials"
-          onChange={e => this.updateInput(e, 'drinkSpecials')}
-        />
-        <p>Food Specials</p>
-        <textarea
-          type="text"
-          value={this.state.foodSpecials}
-          placeholder="Describe Food Specials"
-          onChange={e => this.updateInput(e, 'foodSpecials')}
-        />
-        <button onClick={() => this.submitForm()}>Submit</button>
+          <div className="form-piece drink">
+            <p>Drink Specials</p>
+            <textarea
+              className="drink-specials"
+              type="text"
+              value={this.state.drinkSpecials}
+              placeholder="Describe Drink Specials"
+              onChange={e => this.updateInput(e, 'drinkSpecials')}
+            />
+          </div>
+
+          <div className="form-piece">
+            <p>Food Specials</p>
+            <textarea
+              className="food-specials"
+              type="text"
+              value={this.state.foodSpecials}
+              placeholder="Describe Food Specials"
+              onChange={e => this.updateInput(e, 'foodSpecials')}
+            />
+          </div>
+          <button className="submit-hours" onClick={() => this.submitForm()}>
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
