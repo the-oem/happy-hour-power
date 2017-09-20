0759657b8ad7b0f52b0b004c39bb2db92f0dd357
Daniel Bucket <danieljbucket@gmail.com>
2017-09-20 07:25:20 -0600

    active location reducer test

--- react-ui/src/tests/reducerTests/detailsPageReducer.test.js
+++ react-ui/src/tests/reducerTests/detailsPageReducer.test.js
@@ -5,15 +5,35 @@ import stubDBHappyHours from '../stubData/stubDBHappyHours';
 
 describe('detailsPageReducer', () => {
   it('should return a default state', () => {
-    expect(detailsPageReducer(undefined, {})).toEqual({});
+    expect(detailsPageReducer(undefined, {})).toEqual([]);
   });
 
-  it.only('should update the state with happy hours and locations', () => {
-    const locations = stubDBLocations;
+  it('should update the state with happy hours and locations', () => {
+    const location = [
+      [],
+      {
+        created_at: '2017-09-19T22:18:42.131Z',
+        google_maps_id: '602c42e453cf6eb524c5f0dfaaeaeac49f42e5ff',
+        id: 6,
+        latitude: 39.7478,
+        location_type_id: 3,
+        longitude: -104.993,
+        name: 'Panzano',
+        phone_number: '',
+        updated_at: '2017-09-19T22:18:42.131Z',
+        website_url: ''
+      }
+    ];
+
+    const action = {
+      type: 'UPDATE_DETAIL',
+      location: location
+    };
     const happyhours = stubDBHappyHours;
 
-    expect(
-      detailsPageReducer(Object.assign(locations, happyhours), 'UPDATE_DETAIL')
-    );
+    expect(detailsPageReducer(happyhours, action)).toEqual({
+      happyhours: undefined,
+      location
+    });
   });
 });
